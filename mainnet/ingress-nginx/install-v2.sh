451404f39e59e5a333e7f1cc832177c636f0630b
Austonst <austonst@gmail.com>
2022-11-17 17:38:51 -0700

    Converted all goerli configs to run on DO and mirror the mainnet setup, also some general cleanup

--- mainnet/ingress-nginx/install-v2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Source: https://kubernetes.github.io/ingress-nginx/deploy
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/do/deploy.yaml
-
-kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type json --patch-file nginx-ingress-deployment-tcp-udp-configmap-patch.yml
-kubectl patch service ingress-nginx-controller -n ingress-nginx --type strategic --patch-file nginx-ingress-service-eth-port-proxies-patch.yml
-kubectl patch service ingress-nginx-controller -n ingress-nginx --type strategic --patch-file nginx-ingress-service-lb-hostname-patch.yml
