a4b14bada55f3002e3192f56c05869fa8519b4ee
Austonst <austonst@gmail.com>
2022-12-14 20:33:29 -0700

    Switched loki helm-chart to new maintained source, and created config file

--- monitoring/loki.sh
+++ monitoring/loki.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-helm repo add loki https://grafana.github.io/loki/charts
+helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm upgrade --install loki loki/loki-stack --namespace monitoring --set loki.persistence.enabled=true,loki.persistence.storageClassName=do-block-storage,loki.persistence.size=5Gi
+helm upgrade --install loki grafana/loki-stack --namespace monitoring -f loki_values.yml
