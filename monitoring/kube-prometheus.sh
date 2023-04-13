c7a434c8f527fe5a5fb4b2ed15c3a8d949dbbe40
DigiDr <max@birge.me>
2023-04-13 01:49:10 +0100

    updates to shapella nodes, blockqueue

--- monitoring/kube-prometheus.sh
+++ monitoring/kube-prometheus.sh
@@ -16,3 +16,9 @@ kubectl apply -f manifests/
 
 # Patch to keep metrics-server working
 kubectl patch networkpolicy prometheus-k8s -n monitoring --patch-file prometheus-k8s-patch.yml
+
+#https://github.com/prometheus-operator/prometheus-operator/blob/main/Documentation/additional-scrape-config.md
+kubectl create secret generic additional-scrape-configs --from-file=prometheus-additional.yaml --dry-run=client -oyaml > additional-scrape-configs.yaml
+kubectl apply -f additional-scrape-configs.yaml -n monitoring
+kubectl apply -f manifests/prometheus-prometheus.yaml
+
