486156a1957bb92a29b4beadf899b8fe5e4a5bbc
Austonst <austonst@gmail.com>
2023-04-19 11:35:13 -0600

    ingress-nginx version update, resource patch, and website resource/scaling changes

--- mainnet/ingress-nginx/install.sh
+++ mainnet/ingress-nginx/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Source: https://kubernetes.github.io/ingress-nginx/deploy
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/do/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.0/deploy/static/provider/do/deploy.yaml
 
-kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type json --patch-file nginx-ingress-deployment-tcp-udp-configmap-patch.yml
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type json --patch-file nginx-ingress-deployment-opadd.json
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type json --patch-file nginx-ingress-deployment-opreplace.json
 kubectl patch service ingress-nginx-controller -n ingress-nginx --type strategic --patch-file nginx-ingress-service-eth-port-proxies-patch.yml
 kubectl patch service ingress-nginx-controller -n ingress-nginx --type strategic --patch-file nginx-ingress-service-annotations-patch.yml
 kubectl patch configmap ingress-nginx-controller -n ingress-nginx --type strategic --patch-file nginx-ingress-configmap-config.yml
